d970a764150cf505b1b4ae7d39370dc051789ace
seth_yang <seth_yang@126.com>
2020-01-21 20:26:03 +0800

    隧道基本能用

--- src/main/java/org/dreamwork/tools/network/bridge/client/CliMain.java
+++ src/main/java/org/dreamwork/tools/network/bridge/client/CliMain.java
@@ -5,10 +5,8 @@ import org.dreamwork.app.bootloader.IBootable;
 import org.dreamwork.config.IConfiguration;
 import org.dreamwork.config.PropertyConfiguration;
 import org.dreamwork.db.SQLite;
-import org.dreamwork.network.sshd.Sshd;
-import org.dreamwork.network.sshd.data.SystemConfig;
+import org.dreamwork.network.bridge.data.SystemConfig;
 import org.dreamwork.persistence.DatabaseSchema;
-import org.dreamwork.tools.network.bridge.client.command.ProxyCommand;
 import org.dreamwork.tools.network.bridge.client.data.Proxy;
 import org.dreamwork.tools.network.bridge.client.data.schema.ProxySchema;
 import org.dreamwork.util.StringUtil;
@@ -28,11 +26,15 @@ public class CliMain {
     private final Logger logger = LoggerFactory.getLogger (CliMain.class);
     private static final String sql  = "SELECT _value FROM t_sys_conf WHERE id = ?";
 
+    private IConfiguration root;
+
     public static void main (String[] args) throws InvocationTargetException {
         ApplicationBootloader.run (CliMain.class, args);
     }
 
-    public void start (IConfiguration conf) throws Exception {
+    public void start (IConfiguration conf) {
+        this.root = conf;
+/*
         Sshd sshd = new Sshd ();
         sshd.setConfiguration (conf);
         SQLite sqlite = sshd.initDatabase ();
@@ -40,9 +42,22 @@ public class CliMain {
         sshd.registerCommands (new ProxyCommand (sqlite));
 
         sshd.bind ();
+*/
     }
 
-    private void initDatabase (SQLite sqlite) throws IOException {
+    private void initDatabase (/*SQLite sqlite*/) throws IOException {
+        if (root == null) {
+            logger.error ("root configuration is not found !!");
+            System.exit (-1000);
+            return;
+        }
+
+        boolean sshdDisabled = root.getBoolean ("network.bridge.client.sshd.disabled", false);
+        if (!sshdDisabled) {
+            // sshd server is enabled
+
+        }
+
         DatabaseSchema.register (ProxySchema.class);
         if (!sqlite.isTablePresent (Proxy.class)) {
             try (InputStream in = getClass ().getClassLoader ().getResourceAsStream ("client-schema.sql")) {
@@ -78,6 +93,7 @@ public class CliMain {
             SystemConfig sc = new SystemConfig ();
             sc.setId (keyName);
             sc.setValue (pc.getString (keyName));
+            sc.setEditable (true);
             list.add (sc);
         }
     }
